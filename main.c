5172781d60a6d0226aad1dfd12d0b1cde90a931a
Aden Slade <100041719+SoleBridge@users.noreply.github.com>
2024-12-14 15:11:36 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
- Encrypted Communication using TCP/IP
- Client-Server message application in C with encryption/decryption.
- This demonstrates the use of sockets and OpenSSL AES encryption.
+ Encrypted Communication using sockets and OpenSSL.
+ Client-Server message application with encryption/decryption, authentication.
+ This demonstrates the use of sockets and OpenSSL AES encryption, authentication.
  See: https://wiki.openssl.org/index.php/EVP
 */
 
@@ -10,13 +10,61 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <openssl/evp.h>
-#include <openssl/aes.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 #define PORT            8080
 #define BUFFER_SIZE     1024
 #define AES_KEY_SIZE    256
 #define AES_BLOCK_SIZE  16
+
+// Initializes OpenSSL
+void initialize_openssl() {
+    SSL_load_error_strings();
+    OpenSSL_add_ssl_algorithms();
+}
+
+// Cleans OpenSSL
+void cleanup_openssl() {
+    EVP_cleanup();
+}
+
+// Creates OpenSSL context.
+SSL_CTX *create_context() {
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    method = TLS_server_method();
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    return ctx;
+}
+
+// Reads generated authentiaction files to configure the OpenSSl context.
+void configure_context(SSL_CTX *ctx, const char *cert_file, const char *key_file) {
+	// Load certificate file.
+    if (SSL_CTX_use_certificate_file(ctx, cert_file, SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+	// Load key file.
+    if (SSL_CTX_use_PrivateKey_file(ctx, key_file, SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+	// Check private key.
+    if (!SSL_CTX_check_private_key(ctx)) {
+        fprintf(stderr, "Private key does not match the certificate public key.\n");
+        exit(EXIT_FAILURE);
+    }
+}
 
 // Handles encryption via OpenSSL EVP
 // Encrypts plaintext using AES-256-CBC.
@@ -48,35 +96,7 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
     return ciphertext_len;
 }
 
-// Handles decryption via OpenSSL EVP
-// Decrypts ciphertext using AES-256-CBC.
-// Takes ciphertext, key, IV; writes decrypted message to plaintext.
-// Steps:
-// 1. Create decryption context (EVP_CIPHER_CTX).
-// 2. Initialize decryption using EVP_DecryptInit_ex with AES-256-CBC.
-// 3. Use EVP_DecryptUpdate and EVP_DecryptFinal_ex to decrypt the message.
-// 4. Free decryption context.
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-            unsigned char *iv, unsigned char *plaintext) {
-	int len, plaintext_len;
-
-	// Create decryption context
-    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-    if (!ctx) return -1;
-
-    // Initialize decryption to use AES-256-CBC
-    if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv)) return -1;
-
-    // Decrypt the ciphertext
-    if (1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)) return -1;
-    plaintext_len = len;
-    if (1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) return -1;
-    plaintext_len += len;
-
-    // Free decryption context
-    EVP_CIPHER_CTX_free(ctx);
-    return plaintext_len;
-}
+// Note: decrypt() removed, server uses built in function.
 
 // Starts a server, accepts a connection, and loops to echo clients messages.
 // Steps:
@@ -84,106 +104,108 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
 // 2. Bind socket
 // 3. Listen for connections on socket
 // 4. Accept a connection
+// 5. Try to perform OpenSSL handshake (authentication)
 // 5. Loop to recieve message, decrypt, and respond (echo)
+// 6. Clean up after done with client
 void server() {
-    int server_fd, new_socket;
+    int server_fd;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
 
-    unsigned char key[AES_KEY_SIZE / 8] = "01234567890123456789012345678901"; // 32 bytes key
-    unsigned char iv[AES_BLOCK_SIZE]    = "0123456789012345";                 // 16 bytes IV
+    // Initialize OpenSSL
+    initialize_openssl();
+    SSL_CTX *ctx = create_context();
+    configure_context(ctx, "server.crt", "server.key");
 
-    char buffer[BUFFER_SIZE];
-    unsigned char encrypted[BUFFER_SIZE], decrypted[BUFFER_SIZE];
-
-    // Create socket file descriptor
+    // Create socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket failed");
         exit(EXIT_FAILURE);
     }
-
-	// Set socket settings
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Bind the socket
+	// Bind socket
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("Bind failed");
-        close(server_fd);
         exit(EXIT_FAILURE);
     }
 
-    // Listen for connections
-    if (listen(server_fd, 3) < 0) {
+	// Listen for connection
+    if (listen(server_fd, 1) < 0) {
         perror("Listen failed");
-        close(server_fd);
         exit(EXIT_FAILURE);
     }
-
     printf("Server listening on port %d...\n", PORT);
 
-	// Accept connection(s)
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+	// Accept connection
+    int client_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+    if (client_fd < 0) {
         perror("Accept failed");
-        close(server_fd);
         exit(EXIT_FAILURE);
     }
 
-    while (1) {
-    	// Clear buffers
-        memset(buffer, 0, BUFFER_SIZE);
-        memset(decrypted, 0, BUFFER_SIZE);
+    // Try SSL handshake
+    SSL *ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, client_fd);
 
-        // Get encrypted message from client
-        int valread = read(new_socket, encrypted, BUFFER_SIZE);
-        if (valread <= 0) break;
+    if (SSL_accept(ssl) <= 0) {
+        ERR_print_errors_fp(stderr);
+    } else {
+    	// Conenction successful
+        printf("SSL connection established.\n");
 
-        // Decrypt message
-        int decrypted_len = decrypt(encrypted, valread, key, iv, decrypted);
-        if (decrypted_len < 0) {
-            perror("Decryption failed");
-            break;
+        char buffer[BUFFER_SIZE] = {0};
+        int bytes;
+
+        while ((bytes = SSL_read(ssl, buffer, sizeof(buffer) - 1)) > 0) {
+            buffer[bytes] = '\0';
+            printf("Client: \"%s\"\n", buffer);
+            SSL_write(ssl, buffer, strlen(buffer)); // Echo message
         }
-
-        printf("Client: \"%s\"\n", decrypted);
-
-        // Form and encrypt response
-        snprintf(buffer, BUFFER_SIZE, "Received: \"%s\"", decrypted);
-        int encrypted_len = encrypt((unsigned char *)buffer, strlen(buffer), key, iv, encrypted);
-
-        // Send the encrypted response
-        send(new_socket, encrypted, encrypted_len, 0);
     }
-    // Close sockets
-    close(new_socket);
+
+	// Clean up
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+    close(client_fd);
     close(server_fd);
+
+    SSL_CTX_free(ctx);
+    cleanup_openssl();
 }
 
-// Client main loop
+// Starts a client, tries to connect to server, recieves input, sends to server.
+// Steps:
+// 1. Initialize OpenSSL
+// 2. Create socket, set settings
+// 3. Attempt to connect to server
+// 4. Attempt to authenticate server
+// 5. Loop, get input, encrypt, send to server
+// 5. After done, clean up
 void client(const char *server_ip) {
     int sock;
     struct sockaddr_in serv_addr;
-
     unsigned char key[AES_KEY_SIZE / 8] = "01234567890123456789012345678901"; // 32 bytes key
     unsigned char iv[AES_BLOCK_SIZE] =    "0123456789012345";                 // 16 bytes IV
+	 unsigned char encrypted[BUFFER_SIZE]; // Encrypted message
 
-    char buffer[BUFFER_SIZE];
-    unsigned char encrypted[BUFFER_SIZE], decrypted[BUFFER_SIZE];
-
-	// Default to localhost IP
-    if (!server_ip) {
-        server_ip = "127.0.0.1";
-        printf("No server IP provided. Using default: %s\n", server_ip);
+    // Initialize OpenSSL
+    initialize_openssl();
+    SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
     }
+    SSL *ssl = SSL_new(ctx);
 
 	// Create socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Socket creation error");
         exit(EXIT_FAILURE);
     }
-
-	// Set socket settings
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
     if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0) {
@@ -191,53 +213,64 @@ void client(const char *server_ip) {
         exit(EXIT_FAILURE);
     }
 
-	// Connect to server
+	// Attempt to connect to server
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         perror("Connection failed");
         exit(EXIT_FAILURE);
     }
 
-    while (1) {
-    	// Get message from user
-        printf("Enter message: ");
-        fgets(buffer, BUFFER_SIZE, stdin);
-        buffer[strcspn(buffer, "\n")] = '\0';
+	// Attempt to authenticate server
+    SSL_set_fd(ssl, sock);
+    if (SSL_connect(ssl) <= 0) {
+        ERR_print_errors_fp(stderr);
+    } else { // Authentication success!
+        printf("SSL connection established.\n");
 
-        // Encrypt message
-        int encrypted_len = encrypt((unsigned char *)buffer, strlen(buffer), key, iv, encrypted);
-        if (encrypted_len < 0) {
-            perror("Encryption failed");
-            break;
+        char buffer[BUFFER_SIZE] = {0};
+        while (1) {
+            // Get message from user
+        	printf("> ");
+        	fgets(buffer, BUFFER_SIZE, stdin);
+        	buffer[strcspn(buffer, "\n")] = '\0';
+
+
+			// Encrypt message
+        	int encrypted_len = encrypt((unsigned char *)buffer, strlen(buffer), key, iv, encrypted);
+        	if (encrypted_len < 0) {
+	            perror("Encryption failed");
+    	        break;
+	        }
+
+			// Print encrypted message
+        	printf("Encrypted message: 0x");
+        	for (int i = 0; i < encrypted_len; i++) {
+	            printf("%02x", encrypted[i]);
+        	}
+        	printf("\n");
+
+			// Send message to server
+            SSL_write(ssl, buffer, strlen(buffer)); // Automatically encrypts message
+
+            int bytes = SSL_read(ssl, buffer, sizeof(buffer) - 1);
+            if (bytes > 0) {
+                buffer[bytes] = '\0';
+                printf("Server: %s\n", buffer);
+            } else {
+                break;
+            }
         }
-
-		// Print encrypted message
-        printf("Encrypted message: 0x");
-        for (int i = 0; i < encrypted_len; i++) {
-            printf("%02x", encrypted[i]);
-        }
-        printf("\n");
-
-		// Send message to server
-        send(sock, encrypted, encrypted_len, 0);
-
-		// Read, decrypt server response
-        memset(buffer, 0, BUFFER_SIZE);
-        int valread = read(sock, encrypted, BUFFER_SIZE);
-        if (valread <= 0) break;
-        int decrypted_len = decrypt(encrypted, valread, key, iv, decrypted);
-        if (decrypted_len < 0) {
-            perror("Decryption failed");
-            break;
-        }
-        decrypted[decrypted_len] = '\0';
-
-		// Print server response
-        printf("Server: %s\n", decrypted);
     }
-	// Close socket
+
+	// Clean up
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
     close(sock);
+
+    SSL_CTX_free(ctx);
+    cleanup_openssl();
 }
 
+// Starts a client or a server, depending on CLI args
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <server|client> [server_ip]\n", argv[0]);
@@ -247,10 +280,11 @@ int main(int argc, char *argv[]) {
     if (!strcmp(argv[1], "server")) {
         server();
     } else if (!strcmp(argv[1], "client")) {
-        client(argc == 3 ? argv[2] : NULL);
+        client(argc == 3 ? argv[2] : "127.0.0.1");
     } else {
         fprintf(stderr, "Invalid mode. Use 'server' or 'client'.\n");
         exit(EXIT_FAILURE);
     }
+
     return 0;
 }
